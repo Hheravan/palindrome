fd5dd13e7bad616e3644cd1074e6b0ab67f41e0c
Hashem Emami Heravan <hemami@exiger.com>
2021-05-15 17:42:23 -0400

    changing the findNearestPalindrome parameter to string and fixing the tests and also adding exception test cases

--- src/main/java/com/atb/Palindrome.java
+++ src/main/java/com/atb/Palindrome.java
@@ -1,26 +1,27 @@
 package com.atb;
 /**
  * This class provides a method for checking if a number is palindrome,
- * and find a nearest palindrome number. If there is a tie, returns the smaller one.
+ * and find a nearest palindrome number for a given String representing an integer.
+ * If there is a tie, returns the smaller one.
  * it assumes that all negative numbers are not palindrome and returns 0 for all of them.
- * both methods get and integer number as parameter.
- * parameters will be between Integer.MIN_VALUE and Integer.MAX_VALUE
+ * valid parameters will be between Integer.MIN_VALUE and Integer.MAX_VALUE
  */
 public class Palindrome {
 
 	/**
 	 * This method find a nearest palindrome integer to the given integer number
-	 * @param num  an integer number
+	 * @param input  an String representing a number
 	 * @return     is the nearest palindrome number to num
 	 */
-	public static int findNearestPalindrome(final int num) {
+	public static String findNearestPalindrome(String  input ) {
+		Integer num = Integer.valueOf(input);
 		// Negative numbers are not palindrome and 0 is the nearest  palindrome
-		int result = 0;
+		Integer result = 0;
 		if(num < 0){
-			return result;
+			return result.toString();
 		}
 
-		int counter =1;
+		Integer counter = 1;
 		boolean isPalindrome=false;
 
 		while(!isPalindrome) {
@@ -37,7 +38,7 @@ public class Palindrome {
 				break;
 			}
 		}
-		return result;
+		return result.toString();
 	}
 
 	/**
@@ -46,16 +47,16 @@ public class Palindrome {
 	 * @return true if the given number is palindrome
 	 */
 
-	public static boolean checkPalindrome(final int num){
+	public static boolean checkPalindrome(final Integer num){
 		// Negative numbers are not palindrome and 0 is the nearest  palindrome
 		if ( num < 0 ){
 				return false;
 		}
 
-		int tempNumber = num ;
-		int divider = 1 ;
-		int rightNumber;
-		int leftNumber;
+		Integer tempNumber = num ;
+		Integer divider = 1 ;
+		Integer rightNumber;
+		Integer leftNumber;
 
 		divider = getDivider(tempNumber, divider);
 
@@ -82,7 +83,7 @@ public class Palindrome {
 	 * @return  int
 	 */
 
-	private static int getDivider(int num, int divider) {
+	private static Integer getDivider(Integer num, Integer divider) {
 		// Find largest dividable power of 10 for the number
 		while ( num / divider >= 10 ){
 			divider *= 10;
--- src/test/java/com/atb/PalindromeTest.java
+++ src/test/java/com/atb/PalindromeTest.java
@@ -10,26 +10,25 @@ import static org.junit.Assert.assertEquals;
 public class PalindromeTest {
 	@Test
 	public void testGivenValues(){
-		assertEquals(121, findNearestPalindrome(123));
-		assertEquals(1234444321, findNearestPalindrome(1234554321));
-		assertEquals(1234554321, findNearestPalindrome(1234554329));
-		assertEquals(999999999, findNearestPalindrome(999999998));
-		assertEquals(2147447412, findNearestPalindrome(Integer.MAX_VALUE));
+		assertEquals("121", findNearestPalindrome("123"));
+		assertEquals("1234444321", findNearestPalindrome("1234554321"));
+		assertEquals("1234554321", findNearestPalindrome("1234554329"));
+		assertEquals("999999999", findNearestPalindrome("999999998"));
+		assertEquals("2147447412", findNearestPalindrome(String.valueOf(Integer.MAX_VALUE)));
 	}
 	@Test
 	public void testNotINcludingItself() {
-		assertEquals(11, findNearestPalindrome(12));
-		assertEquals(9, findNearestPalindrome(11));
-		assertEquals(111, findNearestPalindrome(121));
-		assertEquals(0, findNearestPalindrome(1));
-		assertEquals(1, findNearestPalindrome(0));
+		assertEquals("11", findNearestPalindrome("12"));
+		assertEquals("9", findNearestPalindrome("11"));
+		assertEquals("111", findNearestPalindrome("121"));
+		assertEquals("0", findNearestPalindrome("1"));
+		assertEquals("1", findNearestPalindrome("0"));
 	}
 	@Test
 	public void testNegativeValues(){
-		assertEquals(0, findNearestPalindrome(-1000));
-		assertEquals(0, findNearestPalindrome(-1));
-		assertEquals(0, findNearestPalindrome(Integer.MIN_VALUE));
-		assertEquals(true, checkPalindrome(2147447412));
+		assertEquals("0", findNearestPalindrome("-1000"));
+		assertEquals("0", findNearestPalindrome("-1"));
+		assertEquals("2147447412", findNearestPalindrome(String.valueOf(Integer.MAX_VALUE)));
 
 	}
 
@@ -58,5 +57,13 @@ public class PalindromeTest {
 	@Test
 	public void testIsPalindromeOverflowCases(){
 		assertEquals(false, checkPalindrome(299999999));
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void testExceptionCases(){
+		findNearestPalindrome(String.valueOf(Long.MAX_VALUE));
+		findNearestPalindrome(String.valueOf(Long.MIN_VALUE));
+		findNearestPalindrome("-afff333");
+		findNearestPalindrome("-1234231A");
 	}
 }
